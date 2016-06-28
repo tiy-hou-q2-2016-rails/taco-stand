16e813d3f7cd3e2890e7ac676c3f8d3bad7f388c
Jesse Wolgamott <jesse@comalproductions.com>
2016-06-28 12:10:55 -0500

    Update DB seeds based on price validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-
+Taco.delete_all
 Taco.create name: "Al Pastor", price: 5.65, photo_url: "http://www.seriouseats.com/recipes/20120501-204377-tacos-al-pastor-step-8.jpg"
-Taco.create name: "Fish Tacos", price: 8.50, photo_url: "http://www.thenovicechefblog.com/wp-content/uploads/2010/07/DSC_7383-1024x801.jpg"
-Taco.create name: "Beef Fajitas", price: 7.56, photo_url: "http://grouchymuffin.com/wp-content/uploads/2013/05/lupe-tortillas-fajitas.jpg"
+Taco.create name: "Fish Tacos", price: 6.50, photo_url: "http://www.thenovicechefblog.com/wp-content/uploads/2010/07/DSC_7383-1024x801.jpg"
+Taco.create name: "Beef Fajitas", price: 6.56, photo_url: "http://grouchymuffin.com/wp-content/uploads/2013/05/lupe-tortillas-fajitas.jpg"
