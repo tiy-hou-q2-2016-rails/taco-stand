e1a6334a2e2b7ef9de81eb640660145375c9a0fb
Jesse Wolgamott <jesse@comalproductions.com>
2016-06-28 12:48:08 -0500

    Can Add Taco

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#homepage'
 
-  get '/tacos/:id' => 'tacos#show', as: :taco
+  get 'tacos' => 'welcome#homepage', as: :tacos
+  post 'tacos' => 'tacos#create'
+  get 'tacos/new' => 'tacos#new', as: :new_taco
+  get 'tacos/:id' => 'tacos#show', as: :taco
+
 end
