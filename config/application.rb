5e2cdbf7d359429b8abc499c652aef518368bc38
Jesse Wolgamott <jesse@comalproductions.com>
2016-07-18 10:31:50 -0500

    Add SMTP settings for heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module TacoStand
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
   end
 end
